acec230df3b7a734a157a6c6e660633101de04e9
Antonio Facciuti <antonio.facciuti@comm-it.it>
2024-06-12 15:58:07 +0200

    Update calculator.test.js

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -19,3 +19,17 @@ test('Default value of Calculator should be configurable', () => {
 
     expect(calc.result).toEqual(2)
 })
+
+test('Add method should exist', () => {
+    calc = new Calculator();
+
+    expect(calc.add).toBeInstanceOf(Function)
+})
+
+test('Add works correctly', () => {
+    calc = new Calculator(1);
+
+    calc.add(2)
+
+    expect(calc.result).toEqual(3)
+})
